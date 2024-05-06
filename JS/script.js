5749df972b545f3780e241104f962df6f4d93673
ALouKatraz <louise.casteleyn@gmail.com>
2024-05-06 19:18:54 +0200

    Script js pour afficher le nb de jours restants

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,44 @@
-const typed = new Typed('.multiple-text', {
-    strings:    ["géniale", 
-                "motivée",
-                "extraordinaire",
-                "parfaite",
-                "celle qui fait le site",
-                "époustouflante",
-                "modeste",
-                "magnifique",
-                "charismatique",
-                "toujours celle qui fait le site",
-                "lumineuse",
-                "merveilleuse",
-                "remarquable",
-                "talentueuse",
-                "rncore celle qui fait le site",
-                "modeste",
-                "précieuse",
-                "prodigieuse",
-                "généreuse",
-                "",
-                ],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,        
+// Calcul du nombre de jours avant anniversaire
+let date = new Date();
+let date2 = new Date("june 15, 2024 00:00:00 UTC+1");
+
+// différence des jours
+function jour_diff() {
+  let time_dif = date2.getTime() - date.getTime();
+  let days_diff = Math.ceil(time_dif / (1000 * 3600 * 24));
+  return days_diff;
+}
+
+document.getElementById("jours").innerHTML =
+  "Dans " + jour_diff() + " jours, le 15 juin Louise fête son anniversaire!";
+
+
+// texte qui s'écrit seul
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "géniale",
+    "motivée",
+    "extraordinaire",
+    "parfaite",
+    "celle qui fait ce site",
+    "époustouflante",
+    "modeste",
+    "magnifique",
+    "charismatique",
+    "toujours celle qui fait ce site",
+    "lumineuse",
+    "merveilleuse",
+    "remarquable",
+    "talentueuse",
+    "encore celle qui fait ce site",
+    "humble",
+    "précieuse",
+    "prodigieuse",
+    "généreuse",
+    "",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
