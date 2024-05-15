e320095d42fc1de9f8c9220324cb02199d5776d4
ALouKatraz <louise.casteleyn@gmail.com>
2024-05-15 17:25:18 +0200

    Amélioration des pages HTML et CSS +création de la page JS et réalisation d'une fonction permettant l'affichage du texte

---
+++ JS/no.js
@@ -0,0 +1,15 @@
+// texte qui s'écrit seul
+const typed = new Typed(".multiple-text", {
+    strings: [
+      "Non, non mais tout va bien",
+      "je le prends bien",
+      "Je vais avoir 30 ans...",
+      "Je suis grande !",
+      "",
+    ],
+    typeSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true,
+  });
+  
