d80a2873f07f3df8a0801397467653c522421af0
ALouKatraz <louise.casteleyn@gmail.com>
2024-06-03 14:32:34 +0200

    Stylisation de la page no

--- JS/no.js
+++ JS/no.js
@@ -1,15 +1,14 @@
 // texte qui s'écrit seul
 const typed = new Typed(".multiple-text", {
     strings: [
-      "Non, non mais tout va bien",
-      "je le prends bien",
+      "Non, non mais tout va bien...",
+      "Je le prends bien",
       "Je vais avoir 30 ans...",
       "Je suis grande !",
-      "",
     ],
-    typeSpeed: 100,
-    backSpeed: 200,
-    backDelay: 1000,
+    typeSpeed: 50,
+    backSpeed: 75,
+    backDelay: 400,
     loop: true,
   });
   
